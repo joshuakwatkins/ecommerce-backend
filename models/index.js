93f40b865bd587d18058cb661f8f3391149a8b79
Josh Watkins <joshwatkinssfs@gmail.com>
2021-10-23 15:56:32 -0400

    initial files and trial code

---
+++ models/index.js
@@ -0,0 +1,43 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'product.id'
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category.id' 
+});
+
+Product.belongsToMany(Tags, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'Tags'
+})
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'Products with Tag' 
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,43 @@
+ const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Tag, through: ProductTag, as: 'Tags'
+        }
+      ]
+    })
+    if (!tags) {
+      res.status(404).json({ message: "No tags found."})
+    }
+    res.status(200).json(tags)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
